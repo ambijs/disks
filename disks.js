4ef76bae8408cf3b112c8ae7818d862e1a503d6b
Dzintars <dzinis4@inbox.lv>
2015-03-26 21:53:01 +0200

    Pievienots SSTF

--- disks.js
+++ disks.js
@@ -3,8 +3,10 @@ var adata_bidas=[0,0,0,0,0];
 var adata_gaida=[0,0,0,0,0];
 var cilindri=[10,20,30,40,50,60,70,80,90]
 var rinda = [];
-var izpildits = [-1,-1,-1,-1,-1];
+var izpildits = [[],[],[],[],[]];
+var sakums = [-1,-1,-1,-1,-1];
 var dodas = [-1,-1,-1,-1,-1];
+var atrodas = [-1,-1,-1,-1,-1];
 var solis = 1;
 var lasa_datus = 250;
 var cikls = 20;
@@ -12,6 +14,10 @@ var cikls_max=60;
 var cikls_min=5;
 var adatu_bidisana;
 var nobide = 15;
+function notirit()
+{
+	location.reload();
+}
 function atrak()
 {
 	if(cikls>cikls_min)
@@ -28,18 +34,46 @@ function lenak()
 }
 function nakamais_FCFS(a)
 {
-	if (rinda.length > izpildits[a]+1)
+	if (rinda.length > sakums[a]+1)
 	{
-		izpildits[a]=izpildits[a]+1;
-		dodas[a]=izpildits[a];
-		return cilindri[rinda[izpildits[a]]];
+		dodas[a]=sakums[a]+1;
+		sakums[a]=sakums[a]+1;
+		return cilindri[rinda[dodas[a]]];
+	}
+	return -1;
+}
+function nakamais_SSTF(a)
+{
+	if (rinda.length > sakums[a]+1)
+	{
+		var dos = -1;
+		for(var att=0;att<11;att++)
+		{
+			for(var i=sakums[a]+1;i < rinda.length; i++)
+			{
+				if(izpildits[a][i] == 0 && Math.abs(rinda[i]-atrodas[a]) == att)
+				{
+					dos = i;
+					break;
+				}
+			}
+			if (dos != -1)
+			{
+				break;
+			}
+		}
+		if(dos != -1)
+		{
+			dodas[a]=dos;
+			return cilindri[rinda[dodas[a]]];
+		}
 	}
 	return -1;
 }
 function bida_adatas()
 {
 	clearInterval(adatu_bidisana);
-	for(var i=0; i < adata.length; i++)
+	for(var i=0; i <adata.length; i++)
 	{
 		if (adata_gaida[i] > Date.now())
 		{
@@ -47,24 +81,35 @@ function bida_adatas()
 		}
 		else if(adata[i] != adata_bidas[i])
 		{
-			adata[i] = adata[i]-Math.sign(adata[i]-adata_bidas[i])*Math.min(Math.abs(adata[i]-adata_bidas[i]),solis);
+			adata[i] = adata[i]-(Math.sign(adata[i]-adata_bidas[i])*Math.min(Math.abs(adata[i]-adata_bidas[i]),solis));
 			document.getElementById("adata_"+i).style.left = adata[i]+nobide+"px";
 			//alert(adata[i]);
 		}
 		else if(adata[i] == adata_bidas[i])
 		{
+			while(rinda.length > sakums[i]+2 && izpildits[i][sakums[i]+1] == 1)
+			{
+				sakums[i]=sakums[i]+1;
+			};
 			if(dodas[i] != -1)
 			{
 				document.getElementById("m_"+i+"_"+dodas[i]).classList.add("darits");
+				izpildits[i][dodas[i]]=1;
+				atrodas[i]=rinda[dodas[i]];
+				dodas[i]=-1;
 			}
 			var merkis
 			if(i == 0)
 			{
 				merkis = nakamais_FCFS(i);
 			}
+			else if(i == 1)
+			{
+				merkis = nakamais_SSTF(i);
+			}
 			else
 			{
-				merkis = cilindri[Math.round(Math.random()*100)%cilindri.length];
+				merkis = 0;//cilindri[Math.round(Math.random()*100)%cilindri.length];
 			}
 			if(merkis != -1)
 			{
@@ -90,11 +135,21 @@ function pievieno(cilindrs)
 	element.appendChild(para);
 	for(var i=0; i < adata.length; i++)
 	{
+		izpildits[i].push(0);
 		element = document.getElementById("t_p_"+i);
 		para = document.createElement("td");
 		para.id = "m_"+i+"_"+(rinda.length-1);
 		element.appendChild(para);
 	}
+	/*if(cilindrs==8)
+	{
+		alert(izpildits[1]);//1,0
+		alert(sakums[1]);//-1
+		alert(dodas[1]);//1
+		alert(atrodas[1]);//0
+		alert(adata[i]);//nezina
+		alert(adata_bidas[i]);//nezina
+	}*/
 }
 $(document).keyup(function(e) {
 	//alert(e.keyCode);
@@ -108,6 +163,10 @@ $(document).keyup(function(e) {
 	else if(e.keyCode == 187)
 	{
 		atrak();
+	}
+	else if(e.keyCode == 46)
+	{
+		notirit();
 	}
 });
 $(function (){
